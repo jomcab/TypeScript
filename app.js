add6035b2a39f37f1da0e0b23e6b9b86500e85ed
jomercab <jomercabrera@gmail.com>
2022-10-12 14:17:50 +0800

    Working with Enums

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //tuples
+// } = {
+//   name: 'Jomer',
+//   age: 29,
+//   hobbies: ['Working Out', 'Reading'],
+//   role: [2, 'developer']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+// enum Role { ADMIN=100, READ_ONLY=200, AUTHOR=300 }
 var person = {
     name: 'Jomer',
     age: 29,
-    hobbies: ['Working Out', 'Reading'],
-    role: [2, 'developer']
+    hobbies: ['Working out', 'Reading'],
+    role: Role.ADMIN
 };
 //mixed array - not advisable to use everytime
 var favoriteActivities;
@@ -15,4 +33,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('is Admin');
 }
--- app.ts
+++ app.ts
@@ -1,13 +1,23 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //tuples
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //tuples
+// } = {
+//   name: 'Jomer',
+//   age: 29,
+//   hobbies: ['Working Out', 'Reading'],
+//   role: [2, 'developer']
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+// enum Role { ADMIN=100, READ_ONLY=200, AUTHOR=300 }
+
+const person = {
   name: 'Jomer',
   age: 29,
-  hobbies: ['Working Out', 'Reading'],
-  role: [2, 'developer']
+  hobbies: ['Working out', 'Reading'],
+  role: Role.ADMIN
 };
 
 //mixed array - not advisable to use everytime
@@ -23,4 +33,8 @@ console.log(person.name);
 //loop through array
 for (const hobby of person.hobbies){
   console.log(hobby.toUpperCase())
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is Admin');
 }
