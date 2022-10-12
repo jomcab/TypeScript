0488d529c9b409a51bb74ddad217d782e0981497
jomercab <jomercabrera@gmail.com>
2022-10-12 11:19:55 +0800

    Array Types

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var person = {
     name: 'Jomer',
-    age: 29
+    age: 29,
+    hobbies: ['Working Out', 'Reading']
 };
+//mixed array - not advisable to use everytime
+var favoriteActivities;
+favoriteActivities = ['Sports', 1];
+//array of string
+var favoriteActivities2;
+favoriteActivities2 = ['Sports'];
 console.log(person.name);
+//loop through array
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,20 @@
 const person = {
   name: 'Jomer',
-  age: 29
+  age: 29,
+  hobbies: ['Working Out', 'Reading']
 };
 
+//mixed array - not advisable to use everytime
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 1]
+
+//array of string
+let favoriteActivities2: string[];
+favoriteActivities2 = ['Sports']
+
 console.log(person.name);
+
+//loop through array
+for (const hobby of person.hobbies){
+  console.log(hobby.toUpperCase())
+}
