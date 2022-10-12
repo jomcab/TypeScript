3c91aba7aaf5f708b5eda1584477f0bfaacca610
jomercab <jomercabrera@gmail.com>
2022-10-12 15:18:52 +0800

    Type Aliases / Custom Types

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function combine(input1, input2, resultType) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number") {
         result = +input1 + +input2;
     }
     else {
@@ -8,9 +9,9 @@ function combine(input1, input2, resultType) {
     }
     return result;
 }
-var combinedNums = combine(30, 26, 'as-number');
+var combinedNums = combine(30, 26, "as-number");
 console.log(combinedNums);
-var combinedNumsString = combine(30, '26', 'as-number');
+var combinedNumsString = combine(30, "26", "as-number");
 console.log(combinedNumsString);
-var combinedNames = combine('Joms', 'James', 'as-text');
+var combinedNames = combine("Joms", "James", "as-text");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
-function combine(input1: number | string, input2: number | string, resultType: 'as-number' | 'as-text') {
+type NumString = number | string;
+type ResultDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: NumString,
+  input2: NumString,
+  resultType: ResultDescriptor
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultType === "as-number"
+  ) {
     result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
@@ -8,11 +18,11 @@ function combine(input1: number | string, input2: number | string, resultType: '
   return result;
 }
 
-const combinedNums = combine(30, 26, 'as-number');
+const combinedNums = combine(30, 26, "as-number");
 console.log(combinedNums);
 
-const combinedNumsString = combine(30, '26', 'as-number');
+const combinedNumsString = combine(30, "26", "as-number");
 console.log(combinedNumsString);
 
-const combinedNames = combine('Joms', 'James', 'as-text');
-console.log(combinedNames)
+const combinedNames = combine("Joms", "James", "as-text");
+console.log(combinedNames);
