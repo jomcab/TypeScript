5d59d6fcf06c64b3036b0fa15b0f22c7cf477f73
jomercab <jomercabrera@gmail.com>
2022-10-13 14:31:12 +0800

    Class Methods

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,9 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
 }
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
