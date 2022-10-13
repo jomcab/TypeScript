f420b606973a0954b5fa4f18309a20004d460c07
jomercab <jomercabrera@gmail.com>
2022-10-13 22:31:55 +0800

    Interfaces as Function Types

--- dist/app.js
+++ dist/app.js
@@ -11,3 +11,7 @@ class Person {
 let user1;
 user1 = new Person('Jomer');
 console.log(user1);
+let addF;
+addF = (n1, n2) => {
+    return n1 + n2;
+};
--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,15 @@ let user1: Greetable;
 user1 = new Person('Jomer');
 
 console.log(user1);
+
+// type AddFn = (a: number, b: number) => number;
+// Interfaces as Function Types:
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let addF: AddFn;
+
+addF = (n1: number, n2: number) => {
+  return n1 + n2;
+};
