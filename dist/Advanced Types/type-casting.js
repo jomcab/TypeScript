11716dba21303af20b02364b967821cb6bd270ff
jomercab <jomercabrera@gmail.com>
2022-10-14 12:33:04 +0800

    Type Casting

---
+++ dist/Advanced Types/type-casting.js
@@ -0,0 +1,5 @@
+"use strict";
+let userInputElement = document.getElementById('user-input');
+userInputElement.value = 'Hi there!';
+let userInputElement2 = document.getElementById('user-input');
+userInputElement2.value = 'Hi there!';
