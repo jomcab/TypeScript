cddb76201d15eab1473751dafaeedf336bdba34f
jomercab <jomercabrera@gmail.com>
2022-10-14 16:41:09 +0800

    Generic Classes

---
+++ dist/Generics/generic-classes.js
@@ -0,0 +1,24 @@
+"use strict";
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Joms');
+textStorage.addItem('James');
+textStorage.removeItem('Joms');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
