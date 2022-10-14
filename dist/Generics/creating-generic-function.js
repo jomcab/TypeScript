a89691b4d9ae03734fb1c1d5f6bcf86e9e6ca258
jomercab <jomercabrera@gmail.com>
2022-10-14 16:14:35 +0800

    Generic Function with parameter proprty constraint

--- dist/Generics/creating-generic-function.js
+++ dist/Generics/creating-generic-function.js
@@ -1,5 +1,16 @@
 "use strict";
-function displayType(id, name) {
+function displayType2(id, name) {
     console.log(typeof (id) + ", " + typeof (name));
 }
-displayType(1, "Steve");
+displayType2(1, "Steve");
+function countAndDescribe(element) {
+    let descText = 'Got no value.';
+    if (element.length === 1) {
+        descText = 'Got 1 element.';
+    }
+    else if (element.length > 1) {
+        descText = 'Got ' + Element.length + ' elements.';
+    }
+    return [element, descText];
+}
+console.log(countAndDescribe('Hi There!'));
--- src/Generics/creating-generic-function.ts
+++ src/Generics/creating-generic-function.ts
@@ -1,5 +1,23 @@
-function displayType<T, U>(id:T, name:U): void { 
+function displayType2<T, U>(id:T, name:U): void { 
   console.log(typeof(id) + ", " + typeof(name));  
 }
 
-displayType<number, string>(1, "Steve"); // number, string
+displayType2<number, string>(1, "Steve"); // number, string
+
+// Another Generic Function
+interface Properties {
+  length: number;
+}
+
+function countAndDescribe<T extends Properties>(element: T): [T, string] {
+  let descText = 'Got no value.';
+  if (element.length === 1) {
+    descText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descText = 'Got ' + Element.length + ' elements.';
+  }
+
+  return [element, descText];
+}
+
+console.log(countAndDescribe('Hi There!'));
